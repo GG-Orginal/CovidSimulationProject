a1af93d0b3b59f59a63f02f0da34ce3095e56417
Niraj Patel <90847730+NirajPatel2021@users.noreply.github.com>
2021-10-15 12:58:44 -0500

    added Person Class and InitialPopulation Class

---
+++ src/Person.py
@@ -0,0 +1,16 @@
+class Person:
+
+    # This is the indicator for a healthy person (green).
+    is_healthy = None
+    # This is the indicator for an infected person (red).
+    is_infected = None
+    # This is the indicator for a deceased person (black).
+    is_deceased = None
+
+    # This is the indicator for social distancing compliance.
+    is_compliant = None
+
+    # This is the constructor
+    def __init__(self):
+        self
+
