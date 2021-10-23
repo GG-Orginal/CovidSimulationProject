95bbc8846598f6ba0f5c1aef68b32d4b5886b3bd
jvacuda <b2417576@ben.edu>
2021-10-23 17:42:13 -0500

    Person class added attributes
    added immunity attribute

--- src/Person.py
+++ src/Person.py
@@ -18,13 +18,14 @@ class Person:
     # This is the count of non_compliance with social distancing
     non_compliance_counter = 0
 
-    # This is the indicator for social distancing compliance.
-    is_compliant = None
-    age = None
-    gender = None
-    race = None
-    person = None
-    
+    # # This is the indicator for social distancing compliance.
+    # is_compliant = None
+    # age = None
+    # gender = None
+    # race = None
+    # person = None
+    # immune = None
+    # length_of_immunity = 0
 
     # This is the constructor
     def __init__(self, is_healthy=True, is_infected=False, is_deceased=False, is_compliant=True, age=25, gender="M"):
@@ -34,6 +35,9 @@ class Person:
         self.is_compliant = is_compliant
         self.age = age
         self.gender = gender
+        self.immune = False
+        self.length_of_immunity = 0
+        self.length_of_infection = 0
 
     # def __init__(self):
     #     self
@@ -42,11 +46,21 @@ class Person:
         self.is_infected = True
         infected_counter += 1
 
+        #number can be changed later, but set to 10 for now
+        length_of_infection = 10
+
     def changeToHealthy(self):
         self.is_healthy = True
         infected_counter -= 1
+
+
+        #set the healthy person to immune and change the length of their immunity to 20 days 
+        self.immune = True
+        length_of_immunity = 20
 
     def changeToDeceased(self):
         self.is_deceased = True
         self.deceased_counter += 1
         infected_counter -= 1
+
+    
