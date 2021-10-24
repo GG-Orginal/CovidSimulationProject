b50e0ace6b46f89b9d2c78dbf3ea76f1765bf268
Jorge1636 <90848225+Jorge1636@users.noreply.github.com>
2021-10-24 10:55:06 -0500

    Update simulation.py

--- src/simulation.py
+++ src/simulation.py
@@ -3,6 +3,7 @@
 
 from Person import Person
 from random import randrange
+import numpy as np
 
 class simulation: 
 
@@ -38,5 +39,19 @@ class simulation:
             person.is_infected = True
             neighbor.is_infected = True
 
-    
+    def initiatePopulation():
+        #original_population_state = [[Person() for x in range(10)] for y in range (10)]
+        original_population_state = np.empty((10,10), dtype = Person)
+        return original_population_state
+
+    def updateStatus(original_population_state):
+        new_population_state = original_population_state[:]
+        for i in range(len(new_population_state)):
+          for j in range(len(new_population_state[i])):
+             transmit(new_population_state[i][j], new_population_state[i][j+1])
+             transmit(new_population_state[i][j], new_population_state[i][j-1])
+             transmit(new_population_state[i][j], new_population_state[i+1][j])
+             transmit(new_population_state[i][j], new_population_state[i-1][j])
+        original_population_state = new_population_state[:]
+        return original_population_state
             
