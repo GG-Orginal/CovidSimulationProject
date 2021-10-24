833f2ee7ede13117fae393424b01f422a3ee11d2
GGILL <ggill@cosmodigital.com>
2021-10-24 17:31:43 -0500

    GUI and GUIcontroller update

--- src/GUIcontroller.py
+++ src/GUIcontroller.py
@@ -1,11 +1,12 @@
 # Handles the display of backend data onto GUI window
-import Person
+from Person import Person
 import tkinter as tk
 import GUI
+import numpy as np
 
 class PersonPixel:
 
-    def __init__(self, person: Person, size: int, x1: int, y1: int):
+    def __init__(self, person: Person, x1: int, y1: int, size: int):
         self.person = person
         self.color = 'green' if person.is_healthy else ('black' if person.is_deceased else 'red')
         self.size = size
@@ -18,17 +19,17 @@ class PersonPixel:
         GUI.canvas.create_rectangle(self.x1,self.y1,self.x2,self.y2,fill=self.color)
 
 
-def render_snapshot():
-
-    #TODO change to person objects/import from other file
-    people = [[1,1,1],[2,2,2],[3,3,3]]
+def render_snapshot(people):
 
     height = len(people)
     width = len(people[0])
 
     # Render snapshot of current population
-    for i in range(0,height-1,1):
-        for j in range(0,width-1,1):
+    for i in range(0,height,1):
+        for j in range(0,width,1):
 
-    #create pixel objects to pass to GUI
+            #create pixel objects to pass to GUI
+            px = PersonPixel(people[i,j],i*30,j*30,30)
+
+            px.drawPerson()
 
--- src/__main__.py
+++ src/__main__.py
@@ -1,3 +1,13 @@
 from GUI import *
+from GUIcontroller import *
+from InitialPopulation import *
+from simulation import *
+from Person import Person
+
+ppl = InitialPopulation.createPopulation()
+
+render_snapshot(ppl)
+
+updateStatus(ppl)
 
 startProgram()
