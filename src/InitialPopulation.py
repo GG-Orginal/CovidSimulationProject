38cdbbc5960867c791b46e897b77c7cf6f17ff85
Jorge1636 <90848225+Jorge1636@users.noreply.github.com>
2021-11-01 00:30:26 -0500

    Update InitialPopulation.py

--- src/InitialPopulation.py
+++ src/InitialPopulation.py
@@ -3,6 +3,7 @@ from random import randrange
 
 user_input_initial_infection_rate = 5
 user_input_non_compliance_rate = 25
+user_input_vaccination_rate = 10
 user_input_initial_population_size = 20
 
 
@@ -24,6 +25,15 @@ def createPopulation():
         else:
             is_compliant = False
         return is_compliant
+    
+    # This randomly determines if a person is vaccinated or not.
+    def vaccination():
+        value_non_vaccinated = randrange(100) + 1
+        if value_non_vaccinated > user_input_vaccination_rate:
+            is_vaccinated = True
+        else:
+            is_vaccinated = False
+        return is_vaccinated
 
     # This is a list of all the individuals in the sample population
     population_array = [[Person() for j in range(user_input_initial_population_size)]
@@ -37,6 +47,7 @@ def createPopulation():
             p1.is_healthy = initial_health_stage()
             p1.is_infected = not initial_health_stage()
             p1.is_compliant = compliance()
+            p1.is_vaccinated = vaccination()
             population_array[i][j] = p1
     return population_array
 
